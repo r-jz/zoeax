15d8743bd92aee70a94089dbe6cc1999cce8ce31
okamotonr <r.okamoton@gmail.com>
2025-02-13 14:51:28 +0900

    add skelton

--- kernel/src/irq.rs
+++ kernel/src/irq.rs
@@ -1,10 +1,12 @@
-use crate::object::{CSlot, Notification};
+use crate::{object::{CSlot, Notification}, riscv::r_sip, KernelResult};
 use core::mem::MaybeUninit;
 
 const MAX_IRQ: usize = 128; 
 
 static mut IRQ_STATS: [IRQStatus; MAX_IRQ + 1] = [IRQStatus::Inactive; MAX_IRQ + 1];
 static mut IRQ_NODES: MaybeUninit<[CSlot<Notification>; MAX_IRQ + 1]> = MaybeUninit::uninit();
+
+static mut CURRENT_IRQ: Option<usize> = None;
 
 #[derive(Clone, Copy, Debug)]
 pub enum IRQStatus {
@@ -14,7 +16,38 @@ pub enum IRQStatus {
 }
 
 
-pub fn handle_irq(irq_number: usize) -> () {}
+pub unsafe fn handle_irq() {
+    let activate = get_active_irq();
+    if let Ok(irq_num) = activate {
+        if let Some(irq_status) = IRQ_STATS.get(irq_num) {
+            match irq_status {
+                IRQStatus::Inactive => {
+                    // mask irq
+                },
+                IRQStatus::IrqTimer => {
+                    // tick_timer()
+                    // reset_timer()
+                },
+                IRQStatus::IrqSignal => {
+                    let not_cap = IRQ_NODES.assume_init_mut().get_mut(irq_num).unwrap().as_mut().unwrap();
+                    // get notification cap
+                    // send notification
+                }
+            }
+        // exceed max irq
+        } else {
+            // mask irq
+        }
+        // ack irq
+    } else {
+        // mask irq
+    }
+    // 1, get irq number from plic
+    // 2, if irq is to be driven to user process, wake it up
+    // 3, mask irq
+    // 2, if irq number is enabled, tell user process to wake up
+    // 3, else, 
+}
 
 pub unsafe fn init_irq_nodes() {
     let ptr = IRQ_NODES.as_mut_ptr().cast::<CSlot<Notification>>();
@@ -22,3 +55,32 @@ pub unsafe fn init_irq_nodes() {
         *ptr.add(pos) = None
     }
 }
+
+pub fn get_active_irq() -> KernelResult<usize> {
+    unsafe {
+        if let Some(current) = CURRENT_IRQ {
+            Ok(current)
+        } else {
+            todo!();
+            let sip_val = r_sip();
+            let irq = plic_get_irq();
+            // 2, get irq number from plic
+            // -- From seL4 --
+            /* QEMU bug requires external interrupts to be immediately claimed. For
+             * other platforms, the claim is done in invokeIRQHandler_AckIRQ.
+             */
+            plic_complete_claim(irq);
+        }
+    }
+}
+
+pub fn ack_irq(irq_number: usize) {
+    // check irq number is collect
+    unsafe {
+        CURRENT_IRQ = None
+    }
+}
+
+fn plic_get_irq() -> usize {todo!()}
+fn plic_complete_claim(irq: usize) {}
+fn plic_mask_irq(irq: usize) {}
--- kernel/src/trap.rs
+++ kernel/src/trap.rs
@@ -4,11 +4,7 @@ use core::{
 };
 
 use crate::{
-    object::Registers,
-    riscv::{r_scause, r_sepc, r_stval},
-    scheduler::{get_current_reg, get_current_tcb_mut, schedule, timer_tick, CpuVar},
-    syscall::handle_syscall,
-    timer::set_timer,
+    irq::handle_irq, object::Registers, riscv::{r_scause, r_sepc, r_stval}, scheduler::{get_current_reg, get_current_tcb_mut, schedule, timer_tick, CpuVar}, syscall::handle_syscall, timer::set_timer
 };
 
 // I wanna use enum;
@@ -166,10 +162,7 @@ fn handle_trap() -> ! {
                 )
             }
             SUPREVISOREXTERNAL => {
-                panic!(
-                    "supervisor external scause={:x}, stval={:x}, sepc={:x}",
-                    code, stval, user_pc
-                )
+                handle_irq();
             }
             COUNTER_OVERFLOW => {
                 panic!(
